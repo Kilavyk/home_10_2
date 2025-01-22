d06810dae1656193de0b39b41442ecded6907221
Mihail <Kilavyk@yandex.ru>
2025-01-22 23:05:36 +0300

    def mask_account_card finished

--- src/masks.py
+++ src/masks.py
@@ -1,17 +1,14 @@
-def get_mask_card_number(card_number: int) -> str:
+def get_mask_card_number(card_number: str) -> str:
     """Маскирует цифры и разбивает на блоки номер карты"""
-    card_number_str = str(card_number)
-    mask_number = card_number_str[0:6] + "******" + card_number_str[-4:]
-    group_cn_str = mask_number[0:4] + " " + mask_number[4:8] + " " + mask_number[8:12] + " " + mask_number[12:16]
-    return group_cn_str
+    mask_number = card_number[0:6] + "******" + card_number[-4:]
+    group = mask_number[0:4] + " " + mask_number[4:8] + " " + mask_number[8:12] + " " + mask_number[12:16]
+    return group
 
 
-def get_mask_account(account_number: int) -> str:
+def get_mask_account(account_number: str) -> str:
     """Маскирует номер счёта"""
-    account_number_str = str(account_number)
-    mask_account_number_str = "**" + account_number_str[-4:]
-    return mask_account_number_str
+    return "**" + account_number[-4:]
 
 
-print(get_mask_card_number(7000792289606361))
-print(get_mask_account(73654108430135874305))
+#print(get_mask_card_number("7000792289606361"))
+#print(get_mask_account("73654108430135874305"))
--- src/widget.py
+++ src/widget.py
@@ -1,16 +1,19 @@
-import masks
+from src.masks import get_mask_card_number
+from src.masks import get_mask_account
 
-def mask_account_card(numbers: str) -> str:
-    pass
+def mask_account_card(info: str) -> str:
+    """Функция маскирующая счет или номер карты с названием"""
+    number, *name = reversed(info.split(" "))
+    if len(number) == 16:
+        return f"{' '.join(name)} {get_mask_card_number(number)}"
+    else:
+        return f"{' '.join(name)} {get_mask_account(number)}"
 
 
-
-if __name__ == '__main__':
-    print(mask_account_card("Maestro 1596837868705199"))
-    print(mask_account_card("Счет 64686473678894779589"))
-    print(mask_account_card("MasterCard 7158300734726758"))
-    print(mask_account_card("Счет 35383033474447895560"))
-    print(mask_account_card("Visa Classic 6831982476737658"))
-    print(mask_account_card("Visa Platinum 8990922113665229"))
-    print(mask_account_card("Visa Gold 5999414228426353"))
-    print(mask_account_card("Счет 73654108430135874305"))
+print(mask_account_card("Счет 64686473678894779589"))
+print(mask_account_card("MasterCard 7158300734726758"))
+print(mask_account_card("Счет 35383033474447895560"))
+print(mask_account_card("Visa Classic 6831982476737658"))
+print(mask_account_card("Visa Platinum 8990922113665229"))
+print(mask_account_card("Visa Gold 5999414228426353"))
+print(mask_account_card("Счет 73654108430135874305"))
