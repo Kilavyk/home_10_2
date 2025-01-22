bb6cd07c945d2a38e4a64d720c52dddbb2c3718d
Mihail <Kilavyk@yandex.ru>
2025-01-22 23:22:14 +0300

    add def get_date

--- src/widget.py
+++ src/widget.py
@@ -17,3 +17,12 @@ print(mask_account_card("Visa Classic 6831982476737658"))
 print(mask_account_card("Visa Platinum 8990922113665229"))
 print(mask_account_card("Visa Gold 5999414228426353"))
 print(mask_account_card("Счет 73654108430135874305"))
+
+
+def get_date(date_time: str) -> str:
+    """Фунция возвращает день, месяц, год"""
+    date_time = date_time.split("T")
+    year, month, day = date_time[0].split("-")
+    return f"{day}.{month}.{year}"
+
+print(get_date("2024-03-11T02:26:18.671407"))
