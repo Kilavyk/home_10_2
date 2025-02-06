52b6375e263e319c966b4fe2544198f4f30db808
Mihail <Kilavyk@yandex.ru>
2025-02-06 23:47:52 +0300

    add test masks and fix def masks

--- src/masks.py
+++ src/masks.py
@@ -1,5 +1,8 @@
 def get_mask_card_number(card_number: str) -> str:
     """Маскирует цифры и разбивает на блоки номер карты"""
+    card_number = card_number.replace(" ", "")
+    if len(card_number) != 16:
+        return "Неверный номер карты"
     mask_number = card_number[0:6] + "******" + card_number[-4:]
     group = mask_number[0:4] + " " + mask_number[4:8] + " " + mask_number[8:12] + " " + mask_number[12:16]
     return group
@@ -7,8 +10,11 @@ def get_mask_card_number(card_number: str) -> str:
 
 def get_mask_account(account_number: str) -> str:
     """Маскирует номер счёта"""
+    account_number = account_number.replace(" ", "")
+    if len(account_number) != 20:
+        return "Неверный номер счета"
     return "**" + account_number[-4:]
 
 
-# print(get_mask_card_number("7000792289606361"))
+# print(get_mask_card_number("7 0 007 9 22 89 606 361"))
 # print(get_mask_account("73654108430135874305"))
---
+++ tests/test_masks.py
@@ -0,0 +1,19 @@
+import pytest
+
+from src.masks import get_mask_card_number, get_mask_account
+
+
+def test_get_mask_card_number():
+    assert get_mask_card_number('7000792289606361') == '7000 79** **** 6361'
+    assert get_mask_card_number('1234 5678 9012 3456') == '1234 56** **** 3456'
+    assert get_mask_card_number('7 0 007 9 22 89 606 361') == '7000 79** **** 6361'
+    assert get_mask_card_number(' ') == 'Неверный номер карты'
+    assert get_mask_card_number('abcd') == 'Неверный номер карты'
+
+
+
+def test_get_mask_account():
+    assert get_mask_account('73654108430135874305') == '**4305'
+    assert get_mask_account('73654108430135874305123') == 'Неверный номер счета'
+    assert get_mask_account('73654108') == 'Неверный номер счета'
+    assert get_mask_account('7365 410 843 013 587 43 05') == '**4305'
