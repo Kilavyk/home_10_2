dad6fc2feab8e218c745247d8910aba7a08ca13d
Mihail <Kilavyk@yandex.ru>
2025-02-11 22:37:37 +0300

    def card_number_generator done

--- src/generators.py
+++ src/generators.py
@@ -18,13 +18,25 @@ def transaction_descriptions(items):
     for item in items:
         yield item['description']
 
+
+
 descriptions = transaction_descriptions(transactions)
 for operation in range(5):
     print(next(descriptions))
 
 
+def card_number_generator(start, end):
+    for number in range(start, end + 1):
+        card_number = str(number).zfill(16)
+        formatted_card_number = (card_number[0:4]
+                                 + " "
+                                 + card_number[4:8]
+                                 + " "
+                                 + card_number[8:12]
+                                 + " "
+                                 + card_number[12:16])
+        yield formatted_card_number
 
 
-
-def card_number_generator():
-    pass
+for card_number in card_number_generator(1, 15):
+    print(card_number)
