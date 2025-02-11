827f59479ae8f77a5b283d34e87b93697f29f4e4
Mihail <Kilavyk@yandex.ru>
2025-02-11 22:00:13 +0300

    def filter_by_currency done

--- src/generators.py
+++ src/generators.py
@@ -1,6 +1,19 @@
+from sandbox.sandbox import transactions
 
-def filter_by_currency():
-    pass
+
+
+def filter_by_currency(items, cur):
+    for item in items:
+        if cur == item['operationAmount']['currency']['code']:
+            yield item
+
+
+usd_transactions = filter_by_currency(transactions, "RUB")
+for operation in range(2):
+    print(next(usd_transactions))
+
+
+
 
 
 def transaction_descriptions():
