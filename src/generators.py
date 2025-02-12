7d9027a6cd6f8bed4ad6ffef72b6feb5640443a8
Mihail <Kilavyk@yandex.ru>
2025-02-12 19:07:47 +0300

    fixed after linters

--- src/generators.py
+++ src/generators.py
@@ -1,10 +1,88 @@
-from sandbox.sandbox import transactions
+transactions = (
+    [
+        {
+            "id": 939719570,
+            "state": "EXECUTED",
+            "date": "2018-06-30T02:08:58.425572",
+            "operationAmount": {
+                "amount": "9824.07",
+                "currency": {
+                    "name": "USD",
+                    "code": "USD"
+                }
+            },
+            "description": "Перевод организации",
+            "from": "Счет 75106830613657916952",
+            "to": "Счет 11776614605963066702"
+        },
+        {
+            "id": 142264268,
+            "state": "EXECUTED",
+            "date": "2019-04-04T23:20:05.206878",
+            "operationAmount": {
+                "amount": "79114.93",
+                "currency": {
+                    "name": "USD",
+                    "code": "USD"
+                }
+            },
+            "description": "Перевод со счета на счет",
+            "from": "Счет 19708645243227258542",
+            "to": "Счет 75651667383060284188"
+        },
+        {
+            "id": 873106923,
+            "state": "EXECUTED",
+            "date": "2019-03-23T01:09:46.296404",
+            "operationAmount": {
+                "amount": "43318.34",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Перевод со счета на счет",
+            "from": "Счет 44812258784861134719",
+            "to": "Счет 74489636417521191160"
+        },
+        {
+            "id": 895315941,
+            "state": "EXECUTED",
+            "date": "2018-08-19T04:27:37.904916",
+            "operationAmount": {
+                "amount": "56883.54",
+                "currency": {
+                    "name": "USD",
+                    "code": "USD"
+                }
+            },
+            "description": "Перевод с карты на карту",
+            "from": "Visa Classic 6831982476737658",
+            "to": "Visa Platinum 8990922113665229"
+        },
+        {
+            "id": 594226727,
+            "state": "CANCELED",
+            "date": "2018-09-12T21:27:25.241689",
+            "operationAmount": {
+                "amount": "67314.70",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Перевод организации",
+            "from": "Visa Platinum 1246377376343588",
+            "to": "Счет 14211924144426031657"
+        }
+    ]
+)
 
 
-
-def filter_by_currency(items, cur):
+def filter_by_currency(items: list, cur: str):
+    """ Фильтрует транзакции по валюте 'RUB' """
     for item in items:
-        if cur == item['operationAmount']['currency']['code']:
+        if cur == item["operationAmount"]["currency"]["code"]:
             yield item
 
 
@@ -13,11 +91,10 @@ for operation in range(2):
     print(next(usd_transactions))
 
 
-
-def transaction_descriptions(items):
+def transaction_descriptions(items: list[dict[str, object]]):
+    """ Возвращает описание транзакции """
     for item in items:
-        yield item['description']
-
+        yield item["description"]
 
 
 descriptions = transaction_descriptions(transactions)
@@ -25,18 +102,19 @@ for operation in range(5):
     print(next(descriptions))
 
 
-def card_number_generator(start, end):
+def card_number_generator(start: int, end: int):
+    """ Генератор, выдает номера банковских карт в формате ХХХХ ХХХХ ХХХХ ХХХХ """
     for number in range(start, end + 1):
-        card_number = str(number).zfill(16)
-        formatted_card_number = (card_number[0:4]
+        number_card = str(number).zfill(16)
+        formatted_card_number = (number_card[0:4]
                                  + " "
-                                 + card_number[4:8]
+                                 + number_card[4:8]
                                  + " "
-                                 + card_number[8:12]
+                                 + number_card[8:12]
                                  + " "
-                                 + card_number[12:16])
+                                 + number_card[12:16])
         yield formatted_card_number
 
 
-for card_number in card_number_generator(1, 15):
+for card_number in card_number_generator(1, 99):
     print(card_number)
