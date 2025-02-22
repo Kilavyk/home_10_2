d967eb17f391d610f5b318fb561cbbc56f0cc1ab
Mihail <Kilavyk@yandex.ru>
2025-02-22 14:45:49 +0300

    fix decorators.py after linters

--- src/decorators.py
+++ src/decorators.py
@@ -1,10 +1,10 @@
-import os
 from datetime import datetime
 from functools import wraps
 
 
 def log(filename=None):
     """Формирование текст об успешном выполнении функции и возникшие ошибки"""
+
     def decorator(func):
         @wraps(func)  # Сохраняем метаданные оригинальной функции
         def wrapper(*args, **kwargs):
@@ -27,38 +27,37 @@ def write_log(messeng, filename):
     """Записывает сообщение в файл или выводит его в консоль"""
     if filename:
         with open(filename, "a", encoding="utf-8",) as file:
-            file.write(f"{messeng} \n")
+            file.write(f"{messeng}\n")
     else:
         print(messeng)
 
 
 def check_that_agr_is(predicate, error_message):
     """Декоратор для проверки аргументов функции с помощью предиката"""
+
     def wrapper(function):
         @wraps(function)  # Сохраняем метаданные оригинальной функции
         def inner(a, b):
             if not predicate(a, b):
                 raise ValueError(error_message)
-            return function (a, b)
+            return function(a, b)
         return inner
     return wrapper
 
 
-def predicate_is_int(x, y):
+def predicate_is_int(*values):
     """Проверяет, являются ли оба переданных значения целыми числами"""
-    return type(x) == int and type(y) == int
+    return all(isinstance(value, int) for value in values)
 
 
 @log(filename="log.txt")
 @check_that_agr_is(predicate_is_int, "Значения должны быть числом")
-def my_function(x, y):
+def my_function(x: int, y: int) -> int:
     """Возвращает сумму двух чисел"""
     return x + y
 
 
-my_function(1, 2)
-my_function(1, '2')
-
-
-
-
+if __name__ == "__main__":
+    my_function(1, 2)
+    my_function(1, "2")
+    my_function(4, 2)
