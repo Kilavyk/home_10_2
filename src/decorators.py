183c3d7c25c5bbe604df0f54936f6b2af720e2c0
Mihail <Kilavyk@yandex.ru>
2025-02-22 17:39:56 +0300

    add Code coverage

--- src/decorators.py
+++ src/decorators.py
@@ -55,7 +55,7 @@ def my_function(x: int, y: int) -> int:
     return x + y
 
 
-if __name__ == "__main__":
-    my_function(1, 2)
-    my_function(1, "2")
-    my_function(4, 2)
+# if __name__ == "__main__":
+#     my_function(1, 2)
+#     my_function(1, "2")
+#     my_function(4, 2)
