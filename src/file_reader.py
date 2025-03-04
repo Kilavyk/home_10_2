1f1ca17eca62a25f166bd015b20471b68aea5f03
Mihail Sokolov <Kilavyk@yandex.ru>
2025-03-04 20:59:11 +0300

    fix after linters

--- src/file_reader.py
+++ src/file_reader.py
@@ -3,16 +3,16 @@ import os
 import pandas as pd
 
 
-def read_file_scv(input_file):
+def read_file_scv(input_file: str) -> list:
     """Выводит транзакции из файла .csv в виде списка словарей"""
     project_root = os.path.dirname(os.path.dirname(__file__))
     data_file = os.path.join(project_root, "data", input_file)
     with open(data_file, encoding="utf-8") as file:
-        reader = csv.DictReader(file, delimiter=';')
+        reader = csv.DictReader(file, delimiter=";")
         return list(reader)
 
 
-def read_file_xlsx(input_file):
+def read_file_xlsx(input_file: str) -> list:
     """Выводит транзакции из файла .xlsx в виде словарей"""
     project_root = os.path.dirname(os.path.dirname(__file__))
     data_file = os.path.join(project_root, "data", input_file)
@@ -24,4 +24,3 @@ def read_file_xlsx(input_file):
 # if __name__ == '__main__':
 #    print(read_file_scv("transactions.csv"))
 #    print(read_file_xlsx("transactions_excel.xlsx"))
-
