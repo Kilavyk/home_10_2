cbb2c7aa441b935ad93ea4c542417fe64530c4df
Mihail Sokolov <Kilavyk@yandex.ru>
2025-03-04 20:01:59 +0300

    fix bag

--- src/file_reader.py
+++ src/file_reader.py
@@ -3,25 +3,26 @@ import os
 import pandas as pd
 
 
-def read_file_scv():
-    """Выводит транзакции из файла .csv"""
+def read_file_scv(input_file):
+    """Выводит транзакции из файла .csv в виде списка словарей"""
     project_root = os.path.dirname(os.path.dirname(__file__))
-    data_file = os.path.join(project_root, "data/transactions.csv")
+    data_file = os.path.join(project_root, "data", input_file)
     with open(data_file) as file:
-        reader = csv.reader(file, delimiter=';')
+        reader = csv.DictReader(file, delimiter=';')
         for row in reader:
             print(row)
 
 
-def read_file_xlsx():
-    """Выводит транзакции из файла .xlsx"""
+def read_file_xlsx(input_file):
+    """Выводит транзакции из файла .xlsx в виде словарей"""
     project_root = os.path.dirname(os.path.dirname(__file__))
-    data_file = os.path.join(project_root, "data/transactions_excel.xlsx")
-    reader = pd.read_excel(data_file)
+    data_file = os.path.join(project_root, "data", input_file)
+    df = pd.read_excel(data_file)
+    reader = df.to_dict(orient="records")
     return reader
 
 
-if __name__ == '__main__':
-   read_file_scv()
-   print(read_file_xlsx())
+# if __name__ == '__main__':
+#    read_file_scv("transactions.csv")
+#    print(read_file_xlsx("transactions_excel.xlsx"))
 
